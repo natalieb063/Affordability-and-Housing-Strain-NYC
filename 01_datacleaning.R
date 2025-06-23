f32551bc3300fd9c440cee09445872e980bb63bd
Natalie Brown <natalieb063@gmail.com>
2025-06-23 15:47:42 -0400

    Update 01_datacleaning.R

--- 01_datacleaning.R
+++ 01_datacleaning.R
@@ -1,6 +1,6 @@
 
 ###########################################################################################################
-### PROGRAM NAME: Fatherhood Estimate                                                                   ###
+### PROGRAM NAME: 01_DataCleaning.                                                                      ###
 ### DESCRIPTION:  Initial Exploration & Cleaning of IPUMS Data (2023)                                   ###
 ### DATE:         6/22/2025                                                                             ###
 ###########################################################################################################
@@ -18,11 +18,13 @@ nyc_pumas <- pumas(state = "NY", cb = TRUE, year = 2020) %>%
 View(pums_variables)
 
 ny_pums <- get_pums(
-  variables = c("PUMA", "GRPIP","HINCP"),
+  variables = c("PUMA", "GRPIP", "RAC1P", "HISP", "HHT"),
   state = "NY",
     survey = "acs5",
   year = 2023,
   recode = TRUE)
+
+
 
 #n_distinct(nyc_ipums$PUMA) #confirming that all 55 NYC PUMAs are present
 
